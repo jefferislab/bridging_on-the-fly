4f3eae83968ea39393b1c6b6ef58b8727e1df8f6
James Manton <ajd.manton@googlemail.com>
2016-02-02 12:23:42 +0000

    Rewrite to use rglwidget for 3D plots

--- server.R
+++ server.R
@@ -1,191 +1,169 @@
 library(shiny)
 library(nat)
 library(nat.flybrains)
-library(shinyRGL)
-options(nat.default.neuronlist='Cell07PNs')
+library(rglwidget)
 
-# Overwrite RGL's inRows function to reduce the number of digits from 7 to 5
-inRows <- function(values, perrow, leadin="\t", digits=5) {
-  if (is.matrix(values)) values <- t(values)
-  values <- c(values)
-  if (is.numeric(values)) values <- formatC(values, digits = digits, width = 1)
-  len <- length(values)
-  if (len%%perrow != 0) values <- c(values, rep("PADDING", perrow - len%%perrow))
-  values <- matrix(values, ncol = perrow, byrow = TRUE)
-  lines <- paste(leadin, apply(values, 1, function(row) paste(row, collapse = ", ")))
-  lines[length(lines)] <- gsub(", PADDING", "", lines[length(lines)])
-  paste(lines, collapse = ",\n")
-}
-environment(inRows) <- asNamespace('rgl')
-assignInNamespace('inRows', inRows, ns='rgl')
+options(rgl.useNULL=TRUE)
+
 
 # Define a function for a frontal view of the brain
 frontalView<-function(zoom=0.6){
-  um=structure(c(1, 0, 0, 0, 0, -1, 0, 0, 0, 0, -1, 0, 0, 0, 0, 1), .Dim = c(4L, 4L))
-  rgl.viewpoint(userMatrix=um,zoom=zoom)
+	um=structure(c(1, 0, 0, 0, 0, -1, 0, 0, 0, 0, -1, 0, 0, 0, 0, 1), .Dim = c(4L, 4L))
+	rgl.viewpoint(userMatrix=um,zoom=zoom)
 }
 
-
 shinyServer(function(input, output) {
-   TransformStatus <- "PROCESSING"
-   
-   tracing <- reactive({
-    input$bridge_button
-    isolate(query_neuron <- input$file1)
-    if(is.null(query_neuron) | "Choose a brain" %in% c(input$from, input$to)) return(NULL)
-    if(!is.null(reader <- getformatreader(query_neuron$datapath, 'hxsurf')$read)) {
-      tracing_neuron <- reader(query_neuron$datapath)
-    } else if(grepl("\\.zip", query_neuron$name)) {
-      neurons_dir <- file.path(tempdir(), "user_neurons")
-      on.exit(unlink(neurons_dir, recursive=TRUE))
-      unzip(query_neuron$datapath, exdir=neurons_dir)
-      tracing_neuron <- read.neurons(dir(neurons_dir, full=TRUE))
-    }
-    else if(grepl("\\.swc", query_neuron$name)) tracing_neuron <- nat:::read.neuron.swc(query_neuron$datapath)
-    else tracing_neuron <- read.neuron(query_neuron$datapath)
-    
-    tracing_neuron
-  })
-   
-  transformed_tracing <- reactive({
-    tracing_neuron <- tracing()
-    if(is.null(tracing_neuron)) return(NULL)
-    if(input$from != input$to) {
-      if(input$mirror) {
-        tryCatch({
-          tracing_neuron <- mirror_brain(tracing_neuron, get(input$from))
-          tracing_neuron <- xform_brain(tracing_neuron, sample=get(input$from), reference=get(input$to))
-        }, error = function(e) {
-          tryCatch({
-            tracing_neuron <- xform_brain(tracing_neuron, sample=get(input$from), reference=get(input$to))
-            tracing_neuron <- mirror_brain(tracing_neuron, get(input$to))
-          }, error = function(e) {
-            stop("Could not mirror neuron.")
-          })
-        })
-      } else {
-        tracing_neuron <- xform_brain(tracing_neuron, sample=get(input$from), reference=get(input$to))
-      }
-    } else if(input$mirror) {
-      tracing_neuron <- mirror_brain(tracing_neuron, get(input$from))
-    }
-    tracing_neuron
-  })
 
-  
-  # Download points handler
-  output$downloadResults <- downloadHandler(
-    filename = function() {
-      ifelse("hxsurf" %in% class(transformed_tracing()), paste0('transformed_', input$to, '_', input$from, '_', Sys.Date(), '.surf'), paste0('transformed_', Sys.Date(), '.swc'))
-    }, content = function(file) {
-      if("hxsurf" %in% class(transformed_tracing())) {
-        write.hxsurf(transformed_tracing(), file=file)
-      } else {
-        write.neuron(transformed_tracing(), file=file, format="swc", ext=".swc")
-      }
-    }
-  )
-  
-  output$transformedPlot <- renderWebGL({
-    uploadedFile <- transformed_tracing()
-    if(is.null(uploadedFile)) {
-      # Dummy plot
-      spheres3d(5,5,5,0)
-      spheres3d(-5,-5,-5,0)
-      text3d(0,0,0,"Upload a neuron to transform!")
-    } else {
-      TransformStatus <<- "DONE"
-      plot3d(uploadedFile)
-      plot3d(get(paste0(input$to, ".surf")), col="grey", alpha=0.3)
-      frontalView()
-    }
-  })
+###########
+# Tracing #
+###########
+tracing <- reactive({
+	# input$bridge_button
+	isolate(query_neuron <- input$file1)
+	if(is.null(query_neuron) | "Choose a brain" %in% c(input$from, input$to)) return(NULL)
+		if(!is.null(reader <- getformatreader(query_neuron$datapath, 'hxsurf')$read)) {
+			tracing_neuron <- reader(query_neuron$datapath)
+		} else if(grepl("\\.zip", query_neuron$name)) {
+		neurons_dir <- file.path(tempdir(), "user_neurons")
+		on.exit(unlink(neurons_dir, recursive=TRUE))
+		unzip(query_neuron$datapath, exdir=neurons_dir)
+		tracing_neuron <- read.neurons(dir(neurons_dir, full=TRUE))
+	}
+	else if(grepl("\\.swc", query_neuron$name)) tracing_neuron <- nat:::read.neuron.swc(query_neuron$datapath)
+	else tracing_neuron <- read.neuron(query_neuron$datapath)
+	tracing_neuron
+})
 
-  output$originalPlot <- renderWebGL({
-    uploadedFile <- tracing()
-    if(is.null(uploadedFile)) {
-      # Dummy plot
-      spheres3d(5,5,5,0)
-      spheres3d(-5,-5,-5,0)
-      text3d(0,0,0,"Upload a neuron to transform!")
-    } else {
-      plot3d(uploadedFile)
-      plot3d(get(paste0(input$from, ".surf")), col="grey", alpha=0.3)
-      frontalView()
-    }
-  })
-  
-  points <- reactive({
-    pts <- read.table(text=input$input_points)
-    if(ncol(pts)==4) pts <- pts[, 1:3]
-    colnames(pts) <- c('X', 'Y', 'Z')
-    pts
-  })
-  
-  transformed_points <- reactive({
-    pts <- points()
-    if("Choose a brain" %in% c(input$fromPts, input$toPts)) return(NULL)
-    if(input$fromPts != input$toPts) {
-       if(input$mirror_points) {
-        tryCatch({
-          pts <- mirror_brain(pts, get(input$fromPts))
-          pts <- xform_brain(pts, sample=get(input$fromPts), reference=get(input$toPts))
-        }, error = function(e) {
-          tryCatch({
-            pts <- xform_brain(pts, sample=get(input$fromPts), reference=get(input$toPts))
-            pts <- mirror_brain(pts, get(input$toPts))
-          }, error = function(e) {
-            stop("Could not mirror points")
-          })
-        })
-      } else {
-        pts <- xform_brain(pts, sample=get(input$fromPts), reference=get(input$toPts))
-      }
-    }
-    pts
-  })
-  
-  output$originalPtsPlot <- renderWebGL({
-    pts <- points()
-    if(is.null(pts) | "Choose a brain" %in% c(input$fromPts, input$toPts)) {
-      # Dummy plot
-      spheres3d(5,5,5,0)
-      spheres3d(-5,-5,-5,0)
-      text3d(0,0,0,"Enter coordinates in textbox")
-      text3d(0,0,-1.5,"and click bridge!")
-    } else {
-      # Dummy plot
-      spheres3d(pts, radius=5, color=rainbow(length(points())))
-      plot3d(get(paste0(input$fromPts, ".surf")), col="grey", alpha=0.3)
-      frontalView()
-    }
-  })
-  
-  output$transformedPts <- renderTable({
-    pts <- transformed_points()
-    pts
-  })
-  
-  output$transformedPtsPlot <- renderWebGL({
-    pts <- transformed_points()
-    if(is.null(pts) | "Choose a brain" %in% c(input$fromPts, input$toPts)) {
-      # Dummy plot
-      spheres3d(5,5,5,0)
-      spheres3d(-5,-5,-5,0)
-      text3d(0,0,0,"Enter coordinates in textbox")
-      text3d(0,0,-1.5,"and click bridge!")
-    } else {
-      # Dummy plot
-      spheres3d(pts, radius=5, color=rainbow(length(pts)))
-      plot3d(get(paste0(input$toPts, ".surf")), col="grey", alpha=0.3)
-      frontalView()
-    }
-  })
-  
-  output$complete <- reactive({
-    input$bridge_button
-    return(ifelse(TransformStatus=="DONE", TRUE, FALSE))
-  })
-  outputOptions(output, 'complete', suspendWhenHidden=FALSE)
+transformed_tracing <- reactive({
+	tracing_neuron <- tracing()
+	if(is.null(tracing_neuron)) return(NULL)
+	if(input$from != input$to) {
+		if(input$mirror) {
+			tryCatch({
+				tracing_neuron <- mirror_brain(tracing_neuron, get(input$from))
+				tracing_neuron <- xform_brain(tracing_neuron, sample=get(input$from), reference=get(input$to))
+			}, error = function(e) {
+				tryCatch({
+					tracing_neuron <- xform_brain(tracing_neuron, sample=get(input$from), reference=get(input$to))
+					tracing_neuron <- mirror_brain(tracing_neuron, get(input$to))
+				}, error = function(e) {
+					stop("Could not mirror neuron.")
+				})
+			})
+		} else {
+			tracing_neuron <- xform_brain(tracing_neuron, sample=get(input$from), reference=get(input$to))
+		}
+	}
+	tracing_neuron
+})
+
+output$transformedPlot <- renderRglwidget({
+	uploadedFile <- transformed_tracing()
+	clear3d()
+	if(is.null(uploadedFile)) {
+		# Dummy plot
+		spheres3d(5,5,5,0)
+		spheres3d(-5,-5,-5,0)
+		text3d(0,0,0,"Upload a neuron to transform!")
+	} else {
+		TransformStatus <<- "DONE"
+		plot3d(uploadedFile)
+		plot3d(get(paste0(input$to, ".surf")), col="grey", alpha=0.3)
+		frontalView()
+	}
+	rglwidget()
+})
+
+output$originalPlot <- renderRglwidget({
+	uploadedFile <- tracing()
+	clear3d()
+	if(is.null(uploadedFile)) {
+		# Dummy plot
+		spheres3d(5,5,5,0)
+		spheres3d(-5,-5,-5,0)
+		text3d(0,0,0,"Upload a neuron to transform!")
+	} else {
+		plot3d(uploadedFile)
+		plot3d(get(paste0(input$from, ".surf")), col="grey", alpha=0.3)
+		frontalView()
+	}
+	rglwidget()
+})
+
+
+
+##########
+# Points #
+##########
+points <- reactive({
+	pts <- read.table(text=input$input_points)
+	if(ncol(pts)==4) pts <- pts[, 1:3]
+	colnames(pts) <- c('X', 'Y', 'Z')
+	pts
+})
+
+transformed_points <- reactive({
+	pts <- points()
+	if("Choose a brain" %in% c(input$fromPts, input$toPts)) return(NULL)
+	if(input$fromPts != input$toPts) {
+		if(input$mirror_points) {
+			tryCatch({
+				pts <- mirror_brain(pts, get(input$fromPts))
+				pts <- xform_brain(pts, sample=get(input$fromPts), reference=get(input$toPts))
+			}, error = function(e) {
+				tryCatch({
+					pts <- xform_brain(pts, sample=get(input$fromPts), reference=get(input$toPts))
+					pts <- mirror_brain(pts, get(input$toPts))
+				}, error = function(e) {
+					stop("Could not mirror points")
+				})
+			})
+		} else {
+			pts <- xform_brain(pts, sample=get(input$fromPts), reference=get(input$toPts))
+		}
+	}
+	pts
+})
+
+output$originalPtsPlot <- renderRglwidget({
+	pts <- points()
+	clear3d()
+	if(is.null(pts) | "Choose a brain" %in% c(input$fromPts, input$toPts)) {
+		# Dummy plot
+		spheres3d(5,5,5,0)
+		spheres3d(-5,-5,-5,0)
+		text3d(0,0,0,"Enter coordinates in textbox")
+		text3d(0,0.5,0,"and click bridge!")
+	} else {
+		# Dummy plot
+		spheres3d(pts, radius=5, color=rainbow(length(points())))
+		plot3d(get(paste0(input$fromPts, ".surf")), col="grey", alpha=0.3)
+		frontalView()
+	}
+	rglwidget()
+})
+
+output$transformedPts <- renderTable({
+	pts <- transformed_points()
+	pts
+})
+
+output$transformedPtsPlot <- renderRglwidget({
+	pts <- transformed_points()
+	clear3d()
+	if(is.null(pts) | "Choose a brain" %in% c(input$fromPts, input$toPts)) {
+		# Dummy plot
+		spheres3d(5,5,5,0)
+		spheres3d(-5,-5,-5,0)
+		text3d(0,0,0,"Enter coordinates in textbox")
+		text3d(0,0.3,0,"and click bridge!")
+	} else {
+		# Dummy plot
+		spheres3d(pts, radius=5, color=rainbow(length(pts)))
+		plot3d(get(paste0(input$toPts, ".surf")), col="grey", alpha=0.3)
+		frontalView()
+	}
+	rglwidget()
+})
 
 })
--- ui.R
+++ ui.R
@@ -1,101 +1,60 @@
 library(shiny)
-library(nat)
-library(nat.flybrains)
-library(shinyRGL)
+library(rglwidget)
 
-templateList <- list("Choose a brain" = "Choose a brain",
-                     "T1 (Yu et al 2010, Dickson, IMP)" = "T1", 
-                     "IS2 (Cachero, Ostrovsky et al 2010, Jefferis, MRC LMB)" = "IS2", 
-                     "IBNWB (Insect Brain Name Working Group)" = "IBNWB",
-                     "FCWB (Ostrovsky/Costa in prep, Jefferis, MRC LMB)" = "FCWB",
-                     "JFRC2 (HHMI/VFB)" = "JFRC2")
+templateList <- list("Choose a brain" = "Choose a brain", "T1 (Yu et al 2010, Dickson, IMP)" = "T1", "IS2 (Cachero, Ostrovsky et al 2010, Jefferis, MRC LMB)" = "IS2", "IBNWB (Insect Brain Name Working Group)" = "IBNWB", "FCWB (Ostrovsky/Costa in prep, Jefferis, MRC LMB)" = "FCWB", "JFRC2 (HHMI/VFB)" = "JFRC2")
+
+shinyUI(navbarPage("Bridging on-the-fly",
+
+tabPanel("Tracing",
+sidebarLayout(
+	sidebarPanel(
+		h2("Instructions"),
+		HTML("Select your source and target template brains and upload a tracing below. A zip archive of multiple neurons can also be uploaded. The original neuron(s) will be displayed in 3D below, with the bridged neurons to the right."),
+		br(),
+		br(),
+		selectInput("from", "From:", templateList, selected="Choose a brain"),
+		selectInput("to", "To:", templateList, selected="Choose a brain"),
+		checkboxInput("mirror", "Mirror?", value=FALSE),
+		fileInput('file1', 'Neuron/surface file:'),
+		submitButton("Bridge"),
+		HTML('<script type="text/javascript">document.getElementById("bridge_button").onclick = function() { document.getElementById("bridge_button").click(); document.getElementById("bridge_submit").click(); }</script>'),
+
+		h2("Original neuron(s)"),
+		rglwidgetOutput("originalPlot", width="400px", height="400px")
+	),
+
+	mainPanel(
+		h2("Transformed neuron(s)"),
+		rglwidgetOutput("transformedPlot", width="800px", height="800px")
+	)
+)),
 
 
-shinyUI(fluidPage(
-  HTML(
-    '<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-      <div class="container">
-        <div class="navbar-header">
-          <span class="navbar-brand">Bridging on-the-fly</span>
-        </div>
-        <ul class="nav navbar-nav">
-          <li>
-            <a href="#neurons">Bridge neurons</a>
-          </li>
-          <li>
-            <a href="#coordinates">Bridge coordinates</a>
-          </li>
-          <li>
-            <a href="#about">About</a>
-          </li>
-        </ul>
-      </div>
-    </nav>'
-  ),
-  
-  HTML('<a name="neurons"></a>'),
-  h1("Bridge neurons", style="padding-top: 70px;"),
-  sidebarLayout(
-    sidebarPanel(
-      h2("Instructions"),
-      "Select your source and target template brains and upload a tracing below. A zip archive of multiple neurons can also be uploaded. The original neuron(s) will be displayed in 3D below, with the bridged neurons to the right.",
-      br(),
-      br(),
-      selectInput("from", "From:", templateList, selected="Choose a brain"),
-      selectInput("to", "To:", templateList, selected="Choose a brain"),
-      checkboxInput("mirror", "Mirror?", value=FALSE),
-      fileInput('file1', 'Neuron/surface file:'),
-      actionButton("bridge_button", "Bridge", class="btn btn-primary"),
-      tags$button(type='submit', class='btn btn-primary', list(NULL, "Bridge"), id='bridge_submit', style="display: none;"),
-      HTML('<script type="text/javascript">document.getElementById("bridge_button").onclick = function() { document.getElementById("bridge_button").click(); document.getElementById("bridge_submit").click(); }</script>'),
-      h2("Original neuron(s)"),
-      webGLOutput("originalPlot", width="400px", height="400px")
-    ),
-    mainPanel(
-      h2("Transformed neuron(s)"),
-      webGLOutput("transformedPlot", width="800px", height="800px")
-      
-    )
-  ),
-  
-  div(style="height: 100vh;"),
-  
-  HTML('<a name="coordinates"></a>'),
-  h1("Bridge coordinates", style="padding-top: 70px;"),
-  sidebarLayout(
-    sidebarPanel(
-      h2("Instructions"),
-      "Select your source and target template brains and enter 3D coordinates below. The original points will be displayed in 3D below, with the bridged points to the right.",
-      br(),
-      br(),
-      selectInput("fromPts", "From:", templateList, selected="Choose a brain"),
-      selectInput("toPts", "To:", templateList, selected="Choose a brain"),
-      h2("Original points"),
-      HTML('<textarea id="input_points" rows="8" cols=40>100 200 50
+tabPanel("Points",
+sidebarLayout(
+	sidebarPanel(
+		h2("Instructions"),
+		HTML("Select your source and target template brains and enter 3D coordinates below. The original points will be displayed in 3D below, with the bridged points to the right."),
+		br(),
+		br(),
+		selectInput("fromPts", "From:", templateList, selected="Choose a brain"),
+		selectInput("toPts", "To:", templateList, selected="Choose a brain"),
+		h2("Original points"),
+		HTML('<textarea id="input_points" rows="8" cols=40>100 200 50
 400 100 10</textarea>'),
-      checkboxInput("mirror_points", "Mirror?", value=FALSE),
-      submitButton("Bridge"),
-      br(),
-      h3("In 3D"),
-      webGLOutput("originalPtsPlot", width="400px", height="400px")
-    ),
-    
-    mainPanel(
-      h2("Transformed points"),
-      h3("In 3D"),
-      webGLOutput("transformedPtsPlot", width="800px", height="800px"),
-      tableOutput("transformedPts")
-    )
-  ),
-  
-  div(style="height: 100vh;"),
-  
-  HTML('<a name="about"></a>'),
-  h1("About", style="padding-top: 70px;"),
-  HTML("This web app accompanies <a href='http://dx.doi.org/10.1101/006353'>Manton et al. (2014) Combining genome-scale Drosophila 3D neuroanatomical data by bridging template brains</a> and acts as a demonstration of the bridging/mirroring approach for <i>Drosophila</i> brains (as implemented in the R package <a href='https://github.com/jefferislab/nat.flybrains'>nat.flybrains</a>), along with some features of the <a href='https://github.com/jefferis/nat'>NeuroAnatomy Toolbox</a>."),
-  h2("Source code"),
-  HTML("The full code for this web app can be downloaded from <a href='https://github.com/jefferislab/NBLAST_online'>GitHub</a>."),
-  
-  div(style="height: 100vh;")
-  
+		checkboxInput("mirror_points", "Mirror?", value=FALSE),
+		submitButton("Bridge"),
+		br(),
+		h3("In 3D"),
+		rglwidgetOutput("originalPtsPlot", width="400px", height="400px")
+	),
+
+	mainPanel(
+		h2("Transformed points"),
+		h3("In 3D"),
+		rglwidgetOutput("transformedPtsPlot", width="800px", height="800px"),
+		tableOutput("transformedPts")
+	)
+))
+
 ))
